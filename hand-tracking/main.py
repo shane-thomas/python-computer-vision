2635841fc40c7c866694a2e5966694b064453912
shane <shaneshaji10@gmail.com>
2024-01-15 19:38:50 +0530

    changed folder structure

--- hand-tracking/main.py
+++ hand-tracking/main.py
@@ -5,19 +5,28 @@ import time
 cap = cv2.VideoCapture(0)
 
 mpHands = mp.solutions.hands
-hands = mpHands.Hands()
+hands = mpHands.Hands(max_num_hands=4)
 mpDraw = mp.solutions.drawing_utils
 
 previousTime = 0
 currentTime = 0
 
 while True:
-    success, img = cap.read(False)
+    success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
 
     if results.multi_hand_landmarks:  # If hands are detected
         for hand_landmark in results.multi_hand_landmarks:  # For each hand out of all detected hands
+            
+            for id,landmark in enumerate(hand_landmark.landmark):
+                # print(id,landmark)
+                h,w,c = img.shape #Dimensions of the image
+                cx , cy = int(landmark.x*w), int(landmark.y*h) 
+                print(id, cx, cy)
+                # if id == 0:
+                #     cv2.circle(img, (cx,cy), 5, (255, 0, 255), cv2.FILLED)
+
             mpDraw.draw_landmarks(img, hand_landmark, mpHands.HAND_CONNECTIONS)
 
     # displaying the fps
