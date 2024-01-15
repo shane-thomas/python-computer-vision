0bbdf10587ce7a00e15cb790731942058d75cc5d
shane <shaneshaji10@gmail.com>
2024-01-15 20:04:59 +0530

    fixed module instantiation bugs

--- hand-tracking/handTrackingModule.py
+++ hand-tracking/handTrackingModule.py
@@ -5,39 +5,42 @@ import time
 
 class handDetector():
     def __init__(self,
-                 static_image_mode=False,
-                 max_num_hands=2,
-                 min_detection_confidence=0.5,
-                 min_tracking_confidence=0.5):
-        self.mode = static_image_mode
-        self.max = max_num_hands
-        self.detectionCon = min_detection_confidence
-        self.trackCon = min_tracking_confidence
+                 mode=False,
+                 max=2,
+                 complexity=1,
+                 detectionCon=0.5,
+                 trackCon=0.5):
+        self.mode = mode
+        self.max = max
+        self.complexity = complexity
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
 
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(
-            self.mode, self.max, self.detectionCon, self.trackCon)
+        self.hands = self.mpHands.Hands(self.mode, self.max, self.complexity,
+                                        self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
 
 
-def findHands(self, img, draw = True):
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = self.hands.process(imgRGB)
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = self.hands.process(imgRGB)
 
-    if results.multi_hand_landmarks:  # If hands are detected
-        for hand_landmark in results.multi_hand_landmarks:  # For each hand out of all detected hands
-            if draw:
-                self.mpDraw.draw_landmarks(
-                    img, hand_landmark, self.mpHands.HAND_CONNECTIONS)
+        if results.multi_hand_landmarks:  # If hands are detected
+            for hand_landmark in results.multi_hand_landmarks:  # For each hand out of all detected hands
+                if draw:
+                    self.mpDraw.draw_landmarks(
+                        img, hand_landmark, self.mpHands.HAND_CONNECTIONS)
 
-            # for id, landmark in enumerate(hand_landmark.landmark):
-            #     print(id,landmark)
-            #     h, w, c = img.shape  # Dimensions of the image
-            #     cx, cy = int(landmark.x*w), int(landmark.y*h)
-            #     print(id, cx, cy)
-            #     if id == 0:
-            #       cv2.circle(img, (cx,cy), 5, (255, 0, 255), cv2.FILLED)
-    return img
+                # for id, landmark in enumerate(hand_landmark.landmark):
+                #     print(id,landmark)
+                #     h, w, c = img.shape  # Dimensions of the image
+                #     cx, cy = int(landmark.x*w), int(landmark.y*h)
+                #     print(id, cx, cy)
+                #     if id == 0:
+                #       cv2.circle(img, (cx,cy), 5, (255, 0, 255), cv2.FILLED)
+        return img
+
 
 def main():
     previousTime = 0
@@ -47,7 +50,7 @@ def main():
 
     while True:
         success, img = cap.read()
-        img = detector.findHands()
+        img = detector.findHands(img)
         # displaying the fps
         currentTime = time.time()
         fps = 1 / (currentTime-previousTime)
